cc8a05c1f6924965b202516310eff95912523088
aungkomin <jonahaung@gmail.com>
2020-08-21 13:02:50 +0800

    Initial Commit

---
+++ CodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingChallenge
+//
+//  Created by Aung Ko Min on 21/8/20.
+//  Copyright © 2020 Aung Ko Min. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
